c4f2479922c40b330b3f1b544a2e49b79ec9bbf7
Kai Ehrhardt <ehrhardt.kai@gmail.com>
2021-08-02 23:37:38 +0200

    fix cron + current hmip endpoint

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM golang:1.16-alpine as build
 WORKDIR /go/src/app
 COPY . .
 RUN go get -d -v ./... && \
-    go install -v ./...
+    go install -v ./... 
 
 FROM alpine as runtime
+RUN apk add --no-cache tzdata && \
+    cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime && \
+    echo "Europe/Berlin" > /etc/timezone
 COPY --from=build /go/bin/cron-viewer /usr/local/bin/
 ENTRYPOINT ["cron-viewer"]
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func toggle() {
-	resp, err := http.Get("http://api.iobroker.homelab.local/toggle/0_userdata.0.test")
+	resp, err := http.Get("http://api.iobroker.homelab.local/toggle/hmip.0.devices.3014F711A00001DD8993E517.channels.1.on")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,12 +24,18 @@ func toggle() {
 	log.Println(bodyString)
 }
 
+func printCronEntries(cronEntries []cron.Entry) {
+	log.Printf("Cron Info: %+v\n", cronEntries)
+}
+
 func main() {
+	log.Println("Starting...")
 	c := cron.New()
-	c.AddFunc("0 0 4 * * 0-5", toggle)
-	c.AddFunc("0 0 6 * * 0-5", toggle)
-	c.AddFunc("0 0 9 * * 0-5", toggle)
-	c.AddFunc("0 0 22 * * 0-5", toggle)
+	c.AddFunc("0 4 * * *", toggle)
+	c.AddFunc("0 6 * * *", toggle)
+	c.AddFunc("0 9 * * *", toggle)
+	c.AddFunc("0 22 * * *", toggle)
 	c.Start()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	printCronEntries(c.Entries())
+	select {}
 }
